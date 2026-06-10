539f663754a59768eb9e0285573c77cda39c31cc
Harsh Kandpal <harshksndpal1384@gmail.com>
2026-06-10 18:40:34 +0530

    url real representation

--- wWCSV.py
+++ wWCSV.py
@@ -4,6 +4,8 @@ from io import StringIO
 
 #opening a csv file using url
 url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
+# url = ""
+# u cn change url as per ur  need very helpful code snippet
 headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; RV.66.0) Gecko/20100101 Firefox/66.0"}
 req = requests.get(url, headers=headers)
 data = StringIO(req.text)
